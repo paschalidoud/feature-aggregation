38183204b881ac3f5224f5f4dd973ea036951611
Angelos Katharopoulos <katharas@gmail.com>
2017-02-10 03:33:47 +0200

    Add Locality-constrained Linear Coding

--- feature_aggregation/llc.py
+++ feature_aggregation/llc.py
@@ -1,32 +1,30 @@
-"""Quantize local features and aggregate them in a Bag Of Words manner"""
+"""Aggregate local features using Locality-constrained Linear Coding"""
 
 import numpy as np
 from sklearn import cluster
+from sklearn.metrics import pairwise_distances
 
 from base import BaseAggregator
 
 
-class BagOfWords(BaseAggregator):
-    """Compute a Bag of Words model and aggregate local features with it.
+class LLC(BaseAggregator):
+    """Compute a Locality-constrained Linear Coding and aggregate local
+    features with it.
     
-    Train a MiniBatchKMeans on the data and then use the centroids as a
-    codebook to encode any set of local features.
-
     Parameters
     ----------
     n_codewords : int
                   The codebook size aka the number of clusters
-    l1_norm : boolean
-              Whether to normalize the transformed data or not
     dimension_ordering : {'th', 'tf'}
                          Changes how n-dimensional arrays are reshaped to form
                          simple local feature matrices. 'th' ordering means the
                          local feature dimension is the second dimension and
                          'tf' means it is the last dimension.
     """
-    def __init__(self, n_codewords, l1_norm=True, dimension_ordering="tf"):
+    def __init__(self, n_codewords, neighbors=5, beta=1e-4, dimension_ordering="tf"):
         self.n_codewords = n_codewords
-        self.l1_norm = l1_norm
+        self.neighbors = neighbors
+        self.beta = beta
         self._clusterer = cluster.MiniBatchKMeans(
             n_clusters=self.n_codewords,
             n_init=1,
@@ -35,17 +33,8 @@ class BagOfWords(BaseAggregator):
 
         super(self.__class__, self).__init__(dimension_ordering)
 
-    @property
-    def centroids(self):
-        """The centroids of the encoding"""
-        return self._clusterer.cluster_centers_.copy()
-
-    @centroids.setter
-    def centroids(self, _centroids):
-        self._clusterer.cluster_centers_ = _centroids.copy()
-
     def fit(self, X, y=None):
-        """Build the codebook for the Bag of Words model.
+        """Build the codebook for the LLC model.
 
         Apply the clustering algorithm to the data and use the cluster centers
         as codewords for the codebook.
@@ -79,8 +68,8 @@ class BagOfWords(BaseAggregator):
         return self
 
     def transform(self, X):
-        """Compute the Bag of Words representation of the provided data.
-        
+        """Compute the LLC representation of the provided data.
+
         Parameters
         ----------
         X : array_like or list
@@ -95,40 +84,23 @@ class BagOfWords(BaseAggregator):
         starts = np.cumsum([0] + lengths).astype(int)
         ends = np.cumsum(lengths).astype(int)
 
-        # Transform and aggregate the local features
-        words = self._clusterer.predict(X)
-        bow = np.vstack([
-            np.histogram(
-                words[s:e],
-                bins=np.arange(self.n_codewords + 1) - 0.5,
-                density=False
-            )[0]
-            for s, e in zip(starts, ends)
-        ])
+        # Calculate the nearest neighbors
+        centroids = self._clusterer.cluster_centers_
+        distances = pairwise_distances(X, centroids)
+        K = self.neighbors
+        neighbors = np.argpartition(distances, K)[:, :K]
 
-        if self.l1_norm:
-            bow = bow.astype(float) / bow.sum(axis=1).reshape(-1, 1)
+        # Compute the llc representation
+        llc = np.zeros((len(lengths), self.n_codewords))
+        L2 = self.beta * np.eye(X.shape[1])
+        for i, (s, e) in enumerate(zip(starts, ends)):
+            for j in range(s, e):
+                # a = argmin_{1^T a = 1} ||x - Ca||_2^2 + \beta ||a||_2^2
+                C = centroids[neighbors[j]]
+                a = C.dot(np.linalg.inv(C.T.dot(C) + L2)).dot(X[j])
+                llc[i, neighbors[j]] = np.maximum(
+                    llc[i, neighbors[j]],
+                    a / a.sum()
+                )
 
-        return bow
-
-    def inertia(self, X):
-        """Return the value of the KMeans objective function on the provided
-        data.
-
-        X : array_like or list
-            The local features to train on. They must be either nd arrays or
-            a list of nd arrays.
-        """
-        X, _ = self._reshape_local_features(X)
-
-        return -self._clusterer.score(X)
-
-    def score(self, X, y=None):
-        """Return the negative inertia so that the best score is the max
-        score.
-
-        X : array_like or list
-            The local features to train on. They must be either nd arrays or
-            a list of nd arrays.
-        """
-        return -self.inertia(X)
+        return llc
