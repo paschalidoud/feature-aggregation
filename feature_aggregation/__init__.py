2ca6c137360b4bf9da8fb48b74aa84c50c22c7c2
Angelos Katharopoulos <katharas@gmail.com>
2017-02-10 17:31:40 +0200

    Add LLC and Vlad to __init__.py

--- feature_aggregation/__init__.py
+++ feature_aggregation/__init__.py
@@ -1,7 +1,9 @@
 
 from bow import BagOfWords
 from fv import FisherVectors
+from llc import LLC
+from vlad import Vlad
 
 
 __version__ = "0.2"
-__all__ = ["BagOfWords", "FisherVectors"]
+__all__ = ["BagOfWords", "FisherVectors", "LLC", "Vlad"]
